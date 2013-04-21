e5473ad0ce94ad28ddd92e994955424ac13f7232
Hidetoshi Mori <hmori99@gmail.com>
2013-04-21 23:28:40 +0900

    change directory

---
+++ LocationHistory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocationHistory
+//
+//  Created by Hidetoshi Mori on 13/04/21.
+//  Copyright (c) 2013年 Hidetoshi Mori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LHAppDelegate class]));
+    }
+}
